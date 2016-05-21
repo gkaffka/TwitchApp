dfe1a993399bb62d7c0ee1293dc199686b988702
Gabriel Kaffka <gabriel@lovemondays.com.br>
2016-05-21 01:39:09 -0300

    Adds "Pull to refresh"

---
+++ app/src/main/java/com/kaffka/twitchapp/activities/MainActivity.java
@@ -0,0 +1,102 @@
+package com.kaffka.twitchapp.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.kaffka.twitchapp.R;
+import com.kaffka.twitchapp.adapter.TopGamesAdapter;
+import com.kaffka.twitchapp.api_services.TwitchService;
+import com.kaffka.twitchapp.models.GameItem;
+import com.kaffka.twitchapp.models.TopGames;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    @Bind(R.id.swiperefresh)
+    SwipeRefreshLayout swipeRefreshLayout;
+    @Bind(R.id.recycler_view_top_games)
+    RecyclerView recyclerView;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+    @Bind(R.id.progress)
+    ProgressBar progressBar;
+    private List<GameItem> gameItemList;
+    private TopGamesAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        initRecyclerView();
+        initSwipeRefreshLayout();
+        loadGames(50, null);
+    }
+
+    private void initRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        GridLayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        gameItemList = new ArrayList<>();
+        adapter = new TopGamesAdapter(gameItemList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initSwipeRefreshLayout() {
+        swipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+    private void loadGames(Integer limit, Integer offset) {
+        TwitchService.createTwitchService().getTopGames(limit, offset).enqueue(new Callback<TopGames>() {
+            @Override
+            public void onResponse(Call<TopGames> call, Response<TopGames> response) {
+                if (response.isSuccessful())
+                    updateList(response.body().getGameItemList());
+            }
+
+            @Override
+            public void onFailure(Call<TopGames> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void updateList(List<GameItem> list) {
+        gameItemList.clear();
+        gameItemList.addAll(list);
+        adapter.notifyDataSetChanged();
+        progressBar.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        loadGames(50, null);
+    }
+
+    @OnClick(R.id.fab)
+    public void fabClick(View v) {
+        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+}
